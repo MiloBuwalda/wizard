904038c3870fff4bbad993b4800902e80d9e149a
MiloBu <milo.buwalda@gmail.com>
2015-10-13 17:35:31 +0200

    Structure update

---
+++ WizardMultiplayerTest/WizardMultiplayerTest/Assets/CamSwitch.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamSwitch : MonoBehaviour {
+
+	public Camera camMain;
+	public Camera[] camAlt;
+	private bool camMainOn = true;
+
+	// Use this for initialization
+	void Start () {
+		camMain.enabled = camMainOn;
+		camMain.gameObject.SetActive (camMainOn);
+
+		int max = camAlt.Length;
+		for (int i = 0; i < max; i++) {
+			camAlt[i].enabled = !camMainOn;
+			camAlt[i].gameObject.SetActive(!camMainOn);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.C)) {
+			int max = camAlt.Length;
+			camMain.enabled = !camMain.enabled;
+			camMainOn = !camMainOn;
+
+			for (int i = 0; i < max; i++) {
+				camAlt[i].enabled = !camAlt[i].enabled;
+				camAlt[i].gameObject.SetActive(!camMainOn);
+			}
+		}
+	}
+}
--- WizardMultiplayerTest/WizardMultiplayerTest/Assets/_Scripts/SphereInteraction.cs
+++ WizardMultiplayerTest/WizardMultiplayerTest/Assets/_Scripts/SphereInteraction.cs
@@ -6,22 +6,69 @@ public class SphereInteraction : MonoBehaviour {
 	public int element;
 	public HandController handController = null;
 	public bool notGrabbed;
-	public bool shot;
+	public bool shootSign;
+	bool inPosition;
+	bool shot;
+	Vector3 fieldCenter;
+	Vector3 _position;
+	float startTime;
+	float journeyLength;
+	float speed = 1f;
+
+	void Start()
+	{
+		_position = transform.position;
+	}
 
 	void Update()
 	{
-		if (shot == true)
-			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 1 * Time.deltaTime);
+		if (inPosition == true && shootSign == true) 
+		{
+			shot = true;
+		} 
+		else if (inPosition == true && notGrabbed == true && shot == false && _position != fieldCenter) 
+		{
+			float distCovered = (Time.time - startTime) * speed;
+			float fracJourney = distCovered / journeyLength;
+			transform.position = Vector3.Lerp(_position, fieldCenter, fracJourney);
+		}
+		if (shot == true) 
+		{
+			_position = transform.position;
+			//_position.x += Mathf.Sin(Time.time) * Time.deltaTime;
+			_position.z += 3f * Time.deltaTime;
+			transform.position = _position;
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Field") 
+		{
+			fieldCenter = other.gameObject.transform.position;
+		}
 	}
 
 	void OnTriggerStay (Collider other)
 	{
 		if (other.gameObject.tag == "Field" && notGrabbed == true) 
 		{
-			if(shot == false)
+			if(inPosition == false)
 			{
-				shot = true;
-				Debug.Log("Shoot");
+				inPosition = true;
+				_position = transform.position;
+				startTime = Time.time;
+				journeyLength = Vector3.Distance(_position, fieldCenter);
+			}
+		}
+	}
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.tag == "Field") 
+		{
+			if(inPosition == true)
+			{
+				inPosition = false;
 			}
 		}
 	}
