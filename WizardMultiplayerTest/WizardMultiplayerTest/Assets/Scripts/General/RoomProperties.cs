5c1ae64e84d14c9b0f1dc27ebc3584c49b667383
MiloBu <milo.buwalda@gmail.com>
2015-10-14 15:46:37 +0200

    Connect on P NetworkController

---
+++ WizardMultiplayerTest/WizardMultiplayerTest/Assets/Scripts/General/RoomProperties.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Photons Custom Room properties are named in strings. 
+/// By defining them as constants here, I'm making sure that I won't have any spelling errors when using them in different parts of the project
+/// </summary>
+public class RoomProperty
+{
+	public const string StartTime = "PropertyStartTime";
+} 
---
+++ WizardMultiplayerTest/WizardMultiplayerTest/Assets/Scripts/PhotonNetworkStarter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PhotonNetworkStarter : MonoBehaviour {
+
+	public Canvas canvas;
+
+	private Text txt;
+
+	// Use this for initialization
+	void Start () {
+		txt = canvas.GetComponentInChildren<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		txt.CrossFadeAlpha (Mathf.Sin (Time.deltaTime), 1, false);
+		if (PhotonNetwork.connectionState == ConnectionState.Disconnected && Input.GetKeyDown (KeyCode.P) == true) {
+			NetworkController.Instance.Connect();
+			canvas.gameObject.SetActive (false);
+		}
+	}
+}
