182f8f9605f5643c9c0fb6647d89354420411b70
Marwain <marijn_vdbogaard@hotmail.com>
2015-11-12 16:01:14 +0100

    Combine and insideshield

--- WizardOculusLeap/WizardOculusLeap/Assets/_Scripts/ElementSpawner.cs
+++ WizardOculusLeap/WizardOculusLeap/Assets/_Scripts/ElementSpawner.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class ElementSpawner : MonoBehaviour {
 	public static ElementSpawner instance;
 	public Dictionary<string, GameObject> elementBook;
+	int basisNumber;
 
 	void Awake(){
 		//singleton
@@ -27,10 +28,9 @@ public class ElementSpawner : MonoBehaviour {
 		if (elementBook.TryGetValue (t.ToString (), out g)) {
 			element = new ElementManager ();
 			element.instance = (GameObject)Instantiate (g, hands[handNumber].GetPalmPosition(), transform.rotation);
-			//element.instance = (GameObject)Instantiate (g, transform.position, transform.rotation);
 			element.elementType = t;
-			//element.instance.transform.parent = hands[handNumber].transform;
 			element.Setup ();
+			element.elementMovement.handNumber = handNumber;
 		} else {
 			print ("Could not find element: " + t.ToString());
 		}
@@ -52,6 +52,30 @@ public class ElementSpawner : MonoBehaviour {
 		}
 		else if (locationSignX == -1 && location.y < 58.7) {
 			GameManager.instance.player.AddElementToPool (elementType.Earth, handNumber);
+		}
+	}
+
+	public ElementManager CombineElements(List<ElementManager> list){
+		if (list.Count > 0) {
+			ElementManager element = new ElementManager();
+			float speed0 = list[0].instance.GetComponent<ElementMovement>()._magnitude;
+			float speed1 = list[1].instance.GetComponent<ElementMovement>()._magnitude;
+			if(speed0 < speed1){
+				basisNumber = 0; 
+			}else{
+				basisNumber = 1;
+			}
+			ElementManager basis = list[basisNumber];
+			GameObject g;
+			if (elementBook.TryGetValue (basis.elementType.ToString(), out g)){
+				element.instance = (GameObject)Instantiate(g, basis.instance.transform.position, transform.rotation);
+				element.Setup();
+			} 
+			
+			return element;
+		} else {
+			//buzz sound
+			return null;
 		}
 	}
 }
